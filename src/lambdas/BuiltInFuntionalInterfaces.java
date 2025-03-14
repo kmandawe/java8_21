fbc5ed1f11963331358ecb1f1f5ce39a65c9d10e
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-03-14 17:30:02 +0800

    Function and BiFunction

--- src/lambdas/BuiltInFuntionalInterfaces.java
+++ src/lambdas/BuiltInFuntionalInterfaces.java
@@ -1,15 +1,22 @@
 package lambdas;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import javax.crypto.spec.PSource;
 
 public class BuiltInFuntionalInterfaces {
   public static void main(String[] args) {
     BuiltInFuntionalInterfaces fiApi = new BuiltInFuntionalInterfaces();
     fiApi.predicate();
     fiApi.supplier();
+    fiApi.consumer();
   }
 
   public void predicate() {
@@ -29,5 +36,27 @@ public class BuiltInFuntionalInterfaces {
 
     Supplier<Double> sRandom = () -> Math.random();
     System.out.println(sRandom.get());
+  }
+
+  public void consumer() {
+    Consumer<String> printC = s -> System.out.println(s);
+    printC.accept("To be or not to be, that is the question.");
+
+    List<String> names = new ArrayList<>();
+    names.add("John");
+    names.add("Mary");
+    names.forEach(printC);
+
+    var mapCapitalCities = new HashMap<String, String>();
+
+    BiConsumer<String, String> biCon = (key, value) -> mapCapitalCities.put(key, value);
+    biCon.accept("Dublin", "Ireland");
+    biCon.accept("Washington, D.C.", "USA");
+    System.out.println(mapCapitalCities);
+
+    BiConsumer<String, String> mapPrint =
+        (key, value) -> System.out.println(key + " is the capital of : " + value);
+
+    mapCapitalCities.forEach(mapPrint);
   }
 }
