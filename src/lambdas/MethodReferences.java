a5e13f8176243c6f4e26af1d4285f99a3e4a3161
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-03-14 21:25:43 +0800

    Unbound Method References

--- src/lambdas/MethodReferences.java
+++ src/lambdas/MethodReferences.java
@@ -2,6 +2,8 @@ package lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -11,6 +13,7 @@ public class MethodReferences {
     names.forEach(name -> System.out.println(name));
     names.forEach(System.out::println);
     boundMethodReferences();
+    unboundMethodReferences();
   }
 
   public static void boundMethodReferences() {
@@ -27,5 +30,19 @@ public class MethodReferences {
 
     System.out.println(titleR.test("Mr."));
     System.out.println(titleMR.test("Ms."));
+  }
+
+  public static void unboundMethodReferences() {
+    Function<String,String> upperL = s -> s.toUpperCase();
+    Function<String,String> upperMR = String::toUpperCase;
+
+    System.out.println(upperL.apply("sean"));
+    System.out.println(upperMR.apply("sean"));
+
+    BiFunction<String, String, String> concatL = (s1, s2) -> s1.concat(s2);
+    BiFunction<String, String, String> concatMR = String::concat;
+    System.out.println(concatL.apply("Sean", "Kennedy"));
+
+    System.out.println(concatMR.apply("Sean", "Kennedy"));
   }
 }
