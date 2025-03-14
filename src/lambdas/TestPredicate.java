89c4a0b9bfee6d4f8caa85ca8b8d6b4d1bd55b4a
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-03-14 17:05:50 +0800

    Using Predicate

--- src/lambdas/TestPredicate.java
+++ src/lambdas/TestPredicate.java
@@ -14,5 +14,22 @@ public class TestPredicate {
     Predicate<Integer> predicate = i -> i < 0;
     System.out.println("Predicate: " + predicate.test(-1));
     System.out.println("Predicate: " + predicate.test(+1));
+
+    int x = 4;
+    System.out.println("Is " + x + " even? " + check(x, n -> n % 2 == 0));
+    x = 7;
+    System.out.println("Is " + x + " even? " + check(x, n -> n % 2 == 0));
+
+    String name = "Mr. Joe Bloggs";
+    System.out.println(
+        "Does " + name + " start with Mr. ? " + check(name, s -> s.startsWith("Mr.")));
+
+    name = "Ms. Ann Bloggs";
+    System.out.println(
+        "Does " + name + " start with Mr. ? " + check(name, s -> s.startsWith("Mr.")));
+  }
+
+  public static <T> boolean check(T t, Predicate<T> lambda) {
+    return lambda.test(t);
   }
 }
