7ef6f27d072301101b6eeeab82e19b68787ca737
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-03-14 17:20:37 +0800

    Supplier

--- src/lambdas/BuiltInFuntionalInterfaces.java
+++ src/lambdas/BuiltInFuntionalInterfaces.java
@@ -1,12 +1,15 @@
 package lambdas;
 
+import java.time.LocalTime;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class BuiltInFuntionalInterfaces {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     BuiltInFuntionalInterfaces fiApi = new BuiltInFuntionalInterfaces();
     fiApi.predicate();
+    fiApi.supplier();
   }
 
   public void predicate() {
@@ -15,5 +18,16 @@ public class BuiltInFuntionalInterfaces {
 
     BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
     System.out.println(checkLength.test("Vatican City", 8));
+  }
+
+  public void supplier() {
+    Supplier<StringBuilder> supSb = () -> new StringBuilder();
+    System.out.println("Supplier SB: " + supSb.get().append("SK"));
+
+    Supplier<LocalTime> supTime = () -> LocalTime.now();
+    System.out.println("Supplier time: " + supTime.get());
+
+    Supplier<Double> sRandom = () -> Math.random();
+    System.out.println(sRandom.get());
   }
 }
