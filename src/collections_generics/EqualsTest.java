acc035b0fa507001f2d061c5507e3f0e915dd2f1
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-10-08 17:18:18 +0800

    hashCode()

--- src/collections_generics/EqualsTest.java
+++ src/collections_generics/EqualsTest.java
@@ -16,6 +16,19 @@ class Foo {
     return fooValue == foo.fooValue;
   }
 
+  @Override
+  // NB: The contract requires only that two equal objects have equal hashcodes.
+  public int hashCode() {
+    return fooValue * 17; // using the same instance var as equals
+
+    // the following implementation doe NOT violate the contract as two
+    // equal objects will return the same hashcode 100. It is legal and
+    // even correct but horribly inefficient as all objects (including unequal ones,
+    // land on the same bucket). This implementation does not improve
+    // the search time which is what hashcodes are supposed to do.
+    // public int hashCode() { return 100; }
+  }
+
 }
 public class EqualsTest {
   public static void main(String[] args){
@@ -23,5 +36,10 @@ public class EqualsTest {
     Foo f2 = new Foo(2);
     System.out.println(f1.equals(f2));  // true
     System.out.println(f1.equals("SK")); // false (no ClassCastException)
+
+    Foo f3 = new Foo(3);
+    System.out.println(f1.hashCode());
+    System.out.println(f2.hashCode());
+    System.out.println(f3.hashCode());
   }
 }
