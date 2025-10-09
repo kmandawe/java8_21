59b0a920cee75169fef7bd8c0c8d085df7dadd13
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-10-09 19:48:05 +0800

    Submitting Task Collections

---
+++ src/concurrency/SubmittingTaskCollections.java
@@ -0,0 +1,62 @@
+package concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class SubmittingTaskCollections {
+  // Single  Thread Executors will accept tasks sequentially in the order
+  // they are submitted
+//  private static ExecutorService es = Executors.newSingleThreadExecutor();
+  // with 4 threads to share the work, there is no guarantee which letter
+  // will appear first
+  private static ExecutorService es = Executors.newFixedThreadPool(4);
+  private static List<Callable<String>> callables = new ArrayList<>();
+
+  public static void main(String[] args){
+    callables.add(() -> "A");
+    callables.add(() -> "B");
+    callables.add(() -> "C");
+    callables.add(() -> "D");
+
+//    invokeAny();
+    invokeAll();
+  }
+
+  public static void invokeAny(){
+    try {
+      // submitting a collection of tasks
+      // executes syncrhonously and returns when one of the tasks has completed, all
+      // other tasks are cancelled
+      // Note: single thread executor will always execute the first task submitted.
+      String result = es.invokeAny(callables);
+      System.out.println(result);
+    } catch (InterruptedException | ExecutionException ex) {
+      ex.printStackTrace();
+    } finally{
+      es.shutdown();
+    }
+    System.out.println("Always at the end!");
+  }
+
+  public static void invokeAll(){
+    try {
+      // submitting a collection of tasks
+      // executes syncrhonously and returns when all of the tasks are completed
+      // order is maintained i.e. the result for callables.get(0) goes into list.get(0)
+      List<Future<String>> list = es.invokeAll(callables); // TimeUnit overload version exists
+      for (Future<String> future : list) {
+        System.out.println(future.get());
+      }
+    } catch (InterruptedException | ExecutionException ex) {
+      ex.printStackTrace();
+    } finally{
+      es.shutdown();
+    }
+    System.out.println("Always at the end!");
+  }
+}
