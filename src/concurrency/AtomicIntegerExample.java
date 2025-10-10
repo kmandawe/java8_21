39367cf23a880b331c1200e2fec04c7a0c7b6494
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-10-10 09:42:40 +0800

    AtomicInteger

---
+++ src/concurrency/AtomicIntegerExample.java
@@ -0,0 +1,17 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerExample {
+  private static AtomicInteger atomicCount = new AtomicInteger(0);
+
+  public static void main(String[] args){
+    ExecutorService es = Executors.newFixedThreadPool(5);
+    for (int i = 1; i <= 10; i++) {
+      es.submit(() -> System.out.println(atomicCount.incrementAndGet() + " "));
+    }
+    es.shutdown();
+  }
+}
