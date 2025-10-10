00a8869c1af3297f06f1c5cf87ab453d5e9f7605
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-10-10 13:34:02 +0800

    Concurrent Collections - synchronized collections

---
+++ src/concurrency/SynchronizedCollection.java
@@ -0,0 +1,20 @@
+package concurrency;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SynchronizedCollection {
+  public static void main(String[] args){
+    Map<String, String> capitalCities = new HashMap<>();
+    capitalCities.put("Oslo", "Norway");
+    capitalCities.put("Copenhagen", "Denmark");
+
+    Map<String, String> syncCapitalCities = Collections.synchronizedMap(capitalCities);
+    for (String key: syncCapitalCities.keySet()) {
+      System.out.println(key + " is the capital of "+ syncCapitalCities.get(key));
+//      syncCapitalCities.remove(key);
+    }
+  }
+}
