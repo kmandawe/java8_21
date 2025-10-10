8a134a9449192e5e6457cb9a787aac2ba4a5dc8a
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-10-10 11:32:43 +0800

    Concurrent Collections

---
+++ src/concurrency/TheProblem.java
@@ -0,0 +1,19 @@
+package concurrency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TheProblem {
+  public static void main(String[] args){
+//    Map<String, String> capitalCities = new HashMap<>();
+    Map<String, String> capitalCities = new ConcurrentHashMap<>();
+    capitalCities.put("Oslo", "Norway");
+    capitalCities.put("Copenhagen", "Denmark");
+
+    for (String key: capitalCities.keySet()) {
+      System.out.println(key + " is the capital of " + capitalCities.get(key));
+      capitalCities.remove(key);  // ConcurrentModificationException
+    }
+  }
+}
