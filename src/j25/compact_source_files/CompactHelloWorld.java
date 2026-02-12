7089ee2e8183a684b666ad15ffb0ee4d937f56c5
kmandawe <kenneth.mandawe@cheetahdigital.com>
2026-02-12 14:11:55 +0800

    Compact Source Files and Instance Methods

---
+++ src/j25/compact_source_files/CompactHelloWorld.java
@@ -0,0 +1,20 @@
+int x = 5;
+void main() {
+   IO.println("Hello World: " + x);
+  List<String> ls = new ArrayList<>();
+  }
+
+  void main(String[] args) {
+    if (args.length == 0) {
+      main();
+    } else {
+      IO.println("Arguments (" + args.length + "): ");
+      for (var arg : args) {
+        IO.println(" - " + arg);
+      }
+    }
+  }
+
+  void m() {
+  }
+  class AnotherClass {}
---
+++ src/j25/compact_source_files/NonCompactHelloWorld.java
@@ -0,0 +1,11 @@
+package j25.compact_source_files;
+
+
+import module java.base;
+
+public class NonCompactHelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    List<String> ls = new ArrayList<>();
+  }
+}
